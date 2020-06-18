1f6ab996dd5cbdc40bdaf11c4e0f5966d22a6ded
Mory Doumbia <mory.d192@gmail.com>
2020-06-18 00:02:51 -0400

    updated measurement units

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function returnCurrentWeather(cityName) {
 
         currWeatherDiv.html(`
         <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
-        <p>Temperature: ${response.main.temp} &#176;C</p>
+        <p>Temperature: ${response.main.temp} ℉</p>
         <p>Humidity: ${response.main.humidity}%</p>
-        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        <p>Wind Speed: ${response.wind.speed} mph</p>
         `, returnUVIndex(response.coord))
         createHistoryButton(response.name);
     })
@@ -47,7 +47,7 @@ function returnWeatherForecast(cityName) {
                     <div class="card-body">
                         <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
                         <img src=${weatherIcon} alt="Icon">
-                        <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                        <p>Temp: ${forecastInfo[i].main.temp} ℉</p>
                         <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
                     </div>
                 </div>
